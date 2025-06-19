a88420e2334a730387f1a04033793bc6393a48c1
Saikat Talukder <saikat60mu@gmail.com>
2025-06-19 19:16:14 +0600

    Create Longest Common Subsequence.cpp

---
+++ Longest Common Subsequence.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string X, Y;
+    cout << "Enter first string: ";
+    cin >> X;
+    cout << "Enter second string: ";
+    cin >> Y;
+
+    int m = X.length();
+    int n = Y.length();
+
+    int dp[m+1][n+1];
+
+    // DP table initialize
+    for(int i=0; i<=m; i++) {
+        for(int j=0; j<=n; j++) {
+            if(i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if(X[i-1] == Y[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    cout << "Length of LCS: " << dp[m][n] << endl;
+
+    // Optional: print the actual LCS
+    string lcs = "";
+    int i = m, j = n;
+    while(i > 0 && j > 0) {
+        if(X[i-1] == Y[j-1]) {
+            lcs = X[i-1] + lcs;
+            i--; j--;
+        }
+        else if(dp[i-1][j] > dp[i][j-1])
+            i--;
+        else
+            j--;
+    }
+
+    cout << "LCS: " << lcs << endl;
+
+    return 0;
+}
