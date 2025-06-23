e95fc4d968581896046850d980477fab10084df9
Saikat Talukder <saikat60mu@gmail.com>
2025-06-23 20:22:25 +0600

    Update Fractional Knapsack Problem.cpp

--- Fractional Knapsack Problem.cpp
+++ Fractional Knapsack Problem.cpp
@@ -4,52 +4,47 @@ using namespace std;
 struct Item
 {
     string name;
-    double weight,price,unitPrice;
+    double weight, price, unitPrice;
 };
-
 bool comp(Item i1, Item i2)
 {
-    return (i1.unitPrice>i2.unitPrice);
+    return (i1.unitPrice > i2.unitPrice);
 }
-
 int main()
 {
     int n;
     double capacity;
     cout << "Enter the number of items: ";
     cin >> n;
-
     Item arr[n];
-
-    for(int i=0; i<n; i++)
+    cout << "Enter name, weight and price for each item:\n";
+    for (int i = 0; i < n; i++)
     {
         cin >> arr[i].name >> arr[i].weight >> arr[i].price;
-        arr[i].unitPrice = arr[i].price/arr[i].weight;
+        arr[i].unitPrice = arr[i].price / arr[i].weight;
     }
-    sort(arr, arr+n, comp);
-
-    cout << "Enter the capacity of Knapsack : ";
+    sort(arr, arr + n, comp);
+    cout << "Enter the capacity of Knapsack: ";
     cin >> capacity;
 
-    double profit=0;
+    double profit = 0;
 
-    for(int i=0; i<n; i++)
+    cout << "\nSelected items:\n";
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i].weight<=capacity)
+        if (arr[i].weight <= capacity)
         {
-            profit+=arr[i].price;
-            capacity-=arr[i].weight;
+            cout << arr[i].name << " (Full)" << endl;
+            profit += arr[i].price;
+            capacity -= arr[i].weight;
         }
         else
         {
-            profit+=arr[i].unitPrice*capacity;
+            cout << arr[i].name << " (Partial: " << capacity << " out of " << arr[i].weight << ")" << endl;
+            profit += arr[i].unitPrice * capacity;
             break;
         }
     }
-
-    cout << "Profit: " << profit << endl;
-
+    cout << "\nTotal Profit: " << profit << endl;
     return 0;
 }
-
-
